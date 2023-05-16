a72c0e13c31224e2237f5efdfd85efa0c2efa168
Nim Jayawardena <nimjay@google.com>
2023-05-16 13:02:20 -0400

    Improve UX of customize.html
    This also fixes #4.

--- customize.js
+++ customize.js
@@ -5,14 +5,43 @@ async function onSave() {
         return;
     }
     isSaving = true;
+    setSaveStatus('Saving...');
     const textarea = document.getElementById('new-tab-config');
     try {
-        await updateNewTabConfig(JSON.parse(textarea.value))
+        await updateNewTabConfig(JSON.parse(textarea.value));
+        await waitXMilliseconds(800); // For smoother UI.
+        setSaveStatus(`Changes saved at ${getUserFriendlyTime()}.`);
     } catch {
+        setSaveStatus('Failed to save your changes. Double check your punctuation.', true);
         console.error('Failed to save newTabConfig, likely because JSON.parse failed');
     } finally {
         isSaving = false;
     }
+}
+
+function getUserFriendlyTime() {
+    const time = new Date();
+    const config = {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true,
+    };
+    return time.toLocaleString('en-US', config);
+}
+
+function clearSaveStatus() {
+    const saveStatusEl = document.getElementById('save-status');
+    saveStatusEl.classList.remove('save-status-error');
+    saveStatusEl.innerHTML = '';
+}
+
+function setSaveStatus(statusMessage, isError) {
+    const saveStatusEl = document.getElementById('save-status');
+    if (isError) {
+        saveStatusEl.classList.add('save-status-error');
+    }
+    saveStatusEl.innerHTML = statusMessage;
 }
 
 async function renderPage() {
@@ -20,6 +49,14 @@ async function renderPage() {
     const textarea = document.getElementById('new-tab-config');
     textarea.value = JSON.stringify(newTabConfig, null, '    ');
     document.getElementById('save').addEventListener('click', onSave);
+}
+
+function waitXMilliseconds(numOfMilliseconds) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, numOfMilliseconds)
+    });
 }
 
 window.onload = function () {
